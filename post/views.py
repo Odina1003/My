fdf268ad4e68d2b66424cfd10c6b15a031a37dcf
Odina1003 <134951752+Odina1003@users.noreply.github.com>
2023-11-15 03:24:41 +0500

    project

--- post/views.py
+++ post/views.py
@@ -4,5 +4,10 @@ from .models import Post
 
 # Create your views here.
 def post(request):
-    content = Post.objects.all
+    # content = Post.objects.all
+    content = [
+        {'id':'1', 'title':'Blog', 'yaratilgan_sana':'12.11.2023', 'image':'images/blog.jpg'},
+        {'id':'2', 'title':'My_Blog', 'yaratilgan_sana':'2.10.2023', 'image':'images/myblog.jpg'},
+        {'id':'3', 'title':'News', 'yaratilgan_sana':'15.11.2023', 'image':'images/news.jpg'}
+    ]
     return render(request=request, context={'content':content}, template_name='index.html')
